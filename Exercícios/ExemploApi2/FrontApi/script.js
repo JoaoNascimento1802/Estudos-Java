b91b2cee55f5ed6b124fa9c28480e9e99d9ba223
João Emanuel Pessoa do Nascimento <joao.emanuel.pn@gmail.com>
2024-10-14 13:06:41 -0300

    Update script.js

--- Exercícios/ExemploApi2/FrontApi/script.js
+++ Exercícios/ExemploApi2/FrontApi/script.js
@@ -5,17 +5,33 @@ const Isenha = document.querySelector(".senha");
 const Itel = document.querySelector(".tel");
 
 function cadastrar() {
-    
-}
+    fetch("http://localhost:8080/cadastrar", {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify({
+        nome: Inome.value,
+        email: Iemail.value,
+        senha: Isenha.value,
+        tel: Itel.value})
+    })
+    .then(function (res) { console.log(res) })
+    .catch(function (res) { console.log(res) })
+  }
+
+  function limpar(){
+        Inome.value = "";
+        Iemail.value = "";
+        Isenha.value = "";
+        Itel.value = "";
+  }
 
 
 formulario.addEventListener('submit', function (event){
     event.preventDefault();
 
-    const dados = {
-        nome: Inome.value,
-        email: Iemail.value,
-        senha: Isenha.value,
-        tel: Itel.value
-    };
+    cadastrar();
+    limpar();
 });
